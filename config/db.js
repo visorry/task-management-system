52d2938fd6309b70d2b9d50dbb5eb27fe55bd1df
Vishnu R <vishnuvisa00@gmail.com>
2024-06-17 19:03:56 +0530

    Created db.js: Established MongoDB connection using Mongoose

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 const { MONGODB_URI } = require('./dotenv');
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+module.exports = mongoose;
