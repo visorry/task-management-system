008ca7eb8694fb8d634c0dd49eb09ea3fea8eed2
Vishnu R <vishnuvisa00@gmail.com>
2024-06-17 18:30:42 +0530

    Created dotenv.js: Loaded environment variables from .env file

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+const { MONGODB_URI } = require('./dotenv');
 
-mongoose.connect('mongodb+srv://vis:vishnu@cluster0.wcrafar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(MONGODB_URI);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
