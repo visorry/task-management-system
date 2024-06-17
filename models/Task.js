cbca8a549003e1eeb772b0d79b778e6c24c54608
Vishnu R <vishnuvisa00@gmail.com>
2024-06-17 18:45:38 +0530

    Created Task.js: Defined MongoDB schema for Task model

---
+++ models/Task.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  dueDate: {
+    type: Date,
+    required: true
+  },
+  priority: {
+    type: String,
+    enum: ['Low', 'Medium', 'High'],
+    default: 'Medium'
+  },
+  status: {
+    type: String,
+    enum: ['Todo', 'In Progress', 'Done'],
+    default: 'Todo'
+  }
+});
+
+module.exports = mongoose.model('Task', taskSchema);
